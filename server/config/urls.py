030a87d2982570b781040b38c4c85b12f90be500
YaroslavIonin <ioninyaroslav22@gmail.com>
2024-10-29 18:54:19 +0500

    Added version api

--- server/config/urls.py
+++ server/config/urls.py
@@ -8,7 +8,7 @@ from .swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('config.api_urls')),
+    path('api/v1/', include('config.api_urls')),
     path('swagger/',
          schema_view.with_ui(
              'swagger',
